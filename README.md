b37fcfb0597df299657c025e83ff5d82a3dde1c4
Qian Ge <qge2@ncsu.edu>
2018-06-13 14:54:09 -0400

    tune paras

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # recurrent-visual-attention-tensorflow
 A TensorFlow implementation of the recurrent models of visual attention
+
+## Gradient flow
+- Cross entropy loss: train the action network, core network and glimpse network.
+- REINFORCE: train the location network (stop gradient: reward, core network state)
+- MSE loss of baseline: train paramters of baseline estimation
--- example/mnist_center.py
+++ example/mnist_center.py
@@ -17,16 +17,19 @@ from lib.helper.trainer import Trainer
 
 if platform.node() == 'Qians-MacBook-Pro.local':
     DATA_PATH = '/Users/gq/Google Drive/Foram/CNN Data/code/GAN/MNIST_data/'
+    SAVE_PATH = '/Users/gq/tmp/ram/center/'
+    RESULT_PATH = '/Users/gq/tmp/ram/center/result/'
 elif platform.node() == 'arostitan':
     DATA_PATH = '/home/qge2/workspace/data/MNIST_data/'
+    SAVE_PATH = '/home/qge2/workspace/data/out/ram/'
 else:
     DATA_PATH = 'E://Dataset//MNIST//'
+    SAVE_PATH = 'E:/tmp/tmp/'
 
-save_path = 'E:/tmp/tmp/'
-
-BATCH_SIZE = 64
-N_STEP = 6
-N_SAMPLE = 10
+# BATCH_SIZE = 64 
+# N_STEP = 3
+# N_SAMPLE = 10
+# GLIMPSE_SIZE = 12.0
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -34,43 +37,117 @@ def get_args():
                         help='Run prediction')
     parser.add_argument('--train', action='store_true',
                         help='Train the model')
+    parser.add_argument('--test', action='store_true',
+                        help='Test')
+    parser.add_argument('--trans', action='store_true',
+                        help='Transform image')
 
+    parser.add_argument('--step', type=int, default=1,
+                        help='Number of glimpse')
+    parser.add_argument('--sample', type=int, default=1,
+                        help='Number of location samples during training')
+    parser.add_argument('--glimpse', type=int, default=12,
+                        help='Glimpse base size')
+    parser.add_argument('--batch', type=int, default=128,
+                        help='Batch size')
+    parser.add_argument('--epoch', type=int, default=100,
+                        help='Max number of epoch')
+    
     return parser.parse_args()
+
+class config_center():
+    step = 6
+    sample = 1
+    glimpse = 8
+    n_scales = 1
+    batch = 128
+    epoch = 1000
+    loc_std = 0.11
+    unit_pixel = 12
+
+class config_transform():
+    step = 6
+    sample = 2
+    glimpse = 12
+    n_scales = 3
+    batch = 128
+    epoch = 2700
+    loc_std = 0.22
+    unit_pixel = 26
 
 if __name__ == '__main__':
     FLAGS = get_args()
+    if FLAGS.trans:
+        name = 'trans'
+        config = config_transform()
+    else:
+        name = 'centered'
+        config = config_center()
 
     train_data = MNISTData('train', data_dir=DATA_PATH, shuffle=True)
-    train_data.setup(epoch_val=0, batch_size=BATCH_SIZE)
-    # print(train_data.next_batch_dict())
+    train_data.setup(epoch_val=0, batch_size=config.batch)
     valid_data = MNISTData('val', data_dir=DATA_PATH, shuffle=True)
     valid_data.setup(epoch_val=0, batch_size=10)
-    batch_data = valid_data.next_batch_dict()
 
-    model = RAMClassification(im_size=28,
+    model = RAMClassification(
                               im_channel=1,
-                              glimpse_scale=8,
-                              n_loc_sample=N_STEP,
-                              time_step=N_STEP,
+                              glimpse_base_size=config.glimpse,
+                              n_glimpse_scale=config.n_scales,
+                              n_loc_sample=config.sample,
+                              n_step=config.step,
                               n_class=10,
                               max_grad_norm=5.0,
-                              loc_std=0.11)
+                              unit_pixel=config.unit_pixel,
+                              loc_std=config.loc_std,
+                              is_transform=FLAGS.trans)
     model.create_model()
 
     trainer = Trainer(model, train_data)
-    writer = tf.summary.FileWriter(save_path)
+    writer = tf.summary.FileWriter(SAVE_PATH)
     saver = tf.train.Saver()
-    with tf.Session() as sess:
+
+    # gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
+    sessconfig = tf.ConfigProto()
+    sessconfig.gpu_options.allow_growth = True
+    with tf.Session(config=sessconfig) as sess:
         sess.run(tf.global_variables_initializer())
         if FLAGS.train:
-            for step in range(0, 100):
-                trainer.train_epoch(sess, N_SAMPLE)
-                trainer.valid_epoch(sess, valid_data, BATCH_SIZE)
-                saver.save(sess, '{}ram-center-mnist'.format(save_path), global_step=step)
+            writer.add_graph(sess.graph)
+            for step in range(0, config.epoch):
+                trainer.train_epoch(sess, summary_writer=None)
+                trainer.valid_epoch(sess, valid_data, config.batch)
+                saver.save(sess, '{}ram-{}-mnist-step-{}'.format(SAVE_PATH, name, config.step), global_step=step)
         if FLAGS.predict:
-            saver.restore(sess, '{}ram-center-mnist-99'.format(save_path))
-            # writer.add_graph(sess.graph)
-            trainer.test_batch(sess, batch_data, size=8.0)
+            valid_data.setup(epoch_val=0, batch_size=30)
+            saver.restore(sess, '{}ram-centered-mnist-step-6-999'.format(SAVE_PATH))
+            
+            batch_data = valid_data.next_batch_dict()
+            trainer.test_batch(
+                sess,
+                batch_data,
+                unit_pixel=config.unit_pixel,
+                size=config.glimpse,
+                scale=config.n_scales,
+                save_path=RESULT_PATH)
+
+        if FLAGS.test:
+            train_data.setup(epoch_val=0, batch_size=2)
+            batch_data = train_data.next_batch_dict()
+            test, trans_im = sess.run(
+                [model.layers['retina_reprsent'], model.pad_im],
+                feed_dict={model.image: batch_data['data'],
+                           model.label: batch_data['label'],
+                           })
+            # print(test.shape)
+            tt = 0
+            for glimpse_i, trans, im in zip(test, trans_im, batch_data['data']):
+                scipy.misc.imsave('{}trans_{}.png'.format(SAVE_PATH, tt),
+                                  np.squeeze(trans))
+                for idx in range(0, 3):
+                    scipy.misc.imsave('{}g_{}_{}.png'.format(SAVE_PATH, tt, idx), 
+                                      np.squeeze(glimpse_i[0,:,:,idx]))
+                scipy.misc.imsave('{}im_{}.png'.format(SAVE_PATH, tt), np.squeeze(im))
+                tt += 1
         
 
-        writer.close()
+        # writer.close()
