1747a56ecd4b8ae36a42288a316207449874e017
Qian Ge <qge2@ncsu.edu>
2018-07-07 10:26:42 -0400

    upload

--- example/read_mnist.py
+++ example/read_mnist.py
@@ -30,6 +30,31 @@ def translate_image(im, im_size, trans_size):
     trans_im = scipy.ndimage.interpolation.shift(pad_im, translations)
     return trans_im
 
+def translate_image_2(im, im_size, trans_size):
+    pad_h = int(np.ceil((trans_size[0] - im_size[0]) / 2.))
+    pad_w = int(np.ceil((trans_size[1] - im_size[1]) / 2.))
+    pad_im = np.pad(im,
+                    ((pad_h, pad_h), (pad_w, pad_w)),
+                    'constant', constant_values=((0, 0), (0, 0)))
+    trans_h = np.random.random(1) * 2. * pad_h - pad_h
+    trans_w = np.random.random(1) * 2. * pad_w - pad_w
+    trans_im = scipy.ndimage.interpolation.shift(pad_im, (trans_h, trans_w))
+    return trans_im
+
+def put_two_image(im_1, im_2, im_size, canvas_size):
+    pad_size = canvas_size - 2 * im_size
+    assert pad_size >= 0
+    divide_ratio = np.random.random(1)
+    side_1 = int(im_size * 1. * divide_ratio)
+    side_2 = im_size - side_1
+    if np.random.random(1) > 0.5:
+        trans_1 = translate_image_2(im_1, [im_size, im_size], [im_size, side_1])
+        trans_2 = translate_image_2(im_2, [im_size, im_size], [im_size, side_2])
+    else:
+        trans_1 = translate_image_2(im_1, [im_size, im_size], [side_1, im_size])
+        trans_2 = translate_image_2(im_2, [im_size, im_size], [side_2, im_size])
+
+
 def translate_mnist(im):
     return translate_image(im, im_size=28, trans_size=100)
 
